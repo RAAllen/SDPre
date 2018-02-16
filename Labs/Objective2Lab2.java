0f0c30abcadcf5a12e8cb5c5a0eed43b0a5dea04
Rebecca Allen <rebeccazarsky@gmail.com>
2018-02-15 22:37:15 -0700

    add objective 2 labs

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2
+{
+  public static void main(String[] args)
+  {
+    String name = "Rebecca";
+    int age = 36;
+    String state = "New Jersey";
+
+    System.out.print("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
